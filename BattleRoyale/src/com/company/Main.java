dfcdd80d5bdfb8205ae86690b64b6ad83321b89e
highonskooma <joaoguireis99@gmail.com>
2022-06-08 13:05:00 +0100

    cristais spawnam com cores, ao comer o player cresce

--- BattleRoyale/src/com/company/Main.java
+++ BattleRoyale/src/com/company/Main.java
@@ -1,25 +1,40 @@
 package com.company;
-
 import processing.core.PApplet;
 import processing.core.PVector;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main extends PApplet {
-
     public static void main(String[] args) {
 	    PApplet.main("com.company.Main", args);
     }
 
     class Target {
         float tx, ty;
+        int color;
         Target(){
             tx = random(1000);//random x axis position
             ty = random(1000);//random y axis position
+            color = (int) random(3);
         }
         void draw(){
             stroke(100);
+            switch (this.color) {
+                case 0: fill(0,204,102); break;
+                case 1: fill(0,153,255); break;
+                case 2: fill(255,0,0); break;
+            }
             ellipse(tx,ty, radius, radius);//target's location and size
+        }
+
+        public float getX() {return this.tx;}
+        public float getY() {return this.ty;}
+
+        @Override
+        public String toString() {
+            return ("x:"+this.tx+
+                    " , y: "+ this.ty);
         }
     }
 
@@ -27,6 +42,7 @@ public class Main extends PApplet {
     float easing = (float) 0.05;
     PVector circle = new PVector(250, 250);
     int radius = 10;
+    int p_size = 24;
     int lastTargetSpawn;
     int spawnDeltaTime=1000;
     ArrayList<Target> target = new ArrayList<Target>();
@@ -44,23 +60,24 @@ public class Main extends PApplet {
 
     void spawnTarget() {
         target.add(new Target());
-        ellipse(mouseX, mouseY, 24, 24);
         lastTargetSpawn = millis();
     }
 
     public void draw () {
         background(51);
         fill(255);
-        for (int i = 0; i < target.size(); i++) {
-            float x_value = Math.abs(x-target.get(i).tx);
-            float y_value = Math.abs(y-target.get(i).ty);
-            float size = x_value * y_value;
-            if ( size < 10 ) {
-                target.remove(i);
+        text(nf(frameRate, 1, 1)+" FPS", 10, 10);   // show FPS
+        for (Iterator<Target> iterator = target.iterator(); iterator.hasNext(); ) {
+            Target t = iterator.next();
+            float x_value = Math.abs(x-t.getX());
+            float y_value = Math.abs(y-t.getY());
+            float size = x_value * y_value; // diferença absoluta entre as coords do cristal e player
+            if ( size < p_size ) {
+                iterator.remove(); // eat crystal
+                p_size += 5; // size amount to increase
             }
-            target.get(i).draw();
+            t.draw();
         }
-        text(nf(frameRate, 1, 1)+" FPS", 10, 10);   // show your BEAT
 
         PVector m = new PVector(mouseX, mouseY);
 
@@ -68,16 +85,12 @@ public class Main extends PApplet {
         y = y + (m.y - y) * easing;
 
         fill(255, 0, 0);
-        ellipse(x, y, 24, 24);
+        ellipse(x, y, p_size, p_size);
         ellipse(m.x, m.y, 12, 12);
 
-
-
         if (millis() - lastTargetSpawn > spawnDeltaTime) {
-            System.out.println(lastTargetSpawn);
-            spawnTarget();
+            //System.out.println(lastTargetSpawn+" "+ target.toString());
+            spawnTarget(); // spawn crystal every deltaTime
         }
     }
-
-
 }
